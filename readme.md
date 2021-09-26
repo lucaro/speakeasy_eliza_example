285c250a94a6e7801157bd0339720b247caae007
Luca Rossetto <lucaro@Lucaro-Desktop-ZH.localdomain>
2021-09-26 14:58:03 +0200

    simple example client

---
+++ readme.md
@@ -0,0 +1,3 @@
+## Eliza for speakeasy
+
+A simple example client for 'speakeasy' using an 'Eliza' implementation based on https://github.com/codeanticode/eliza.
---
+++ src/main/kotlin/ch/lucaro/speakeasyexample/Main.kt
@@ -0,0 +1,95 @@
+package ch.lucaro.speakeasyexample
+
+import ch.ddis.speakeasy.client.ChatApi
+import ch.ddis.speakeasy.client.UserApi
+import codeanticode.eliza.Eliza
+import org.openapitools.client.infrastructure.ClientException
+import org.openapitools.client.models.ChatRoomInfo
+import org.openapitools.client.models.LoginRequest
+
+object Main {
+
+    val basePath = "https://speakeasy.ifi.uzh.ch"
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        if (args.size < 2) {
+            println("expected user and password as parameters")
+            return
+        }
+
+        val user = args[0]
+        val password = args[1]
+
+        val userApi = UserApi(basePath)
+        val chatApi = ChatApi(basePath)
+
+        val sessionDetails = userApi.postApiLogin(
+            LoginRequest(
+                user, password
+            )
+        )
+
+        println(sessionDetails)
+
+        val knownRooms = mutableSetOf<String>()
+
+        while (true) {
+            val rooms = chatApi.getApiRooms(sessionDetails.sessionToken).rooms.filter { it.remainingTime > 0 }
+
+            rooms.forEach { room ->
+                if (room.uid !in knownRooms) {
+                    println("new chat room: ${room}")
+
+                    knownRooms.add(room.uid)
+
+                    Thread {
+
+                        val roomId = room.uid
+                        var active = true
+                        var lastMessageTime = 0L
+
+                        val eliza = Eliza()
+
+                        while (active) {
+
+                            try{
+
+                            val state = chatApi.getApiRoomWithRoomidWithSince(
+                                roomId,
+                                lastMessageTime + 1,
+                                sessionDetails.sessionToken
+                            )
+
+                            val lastMessage =
+                                state.messages.filter { it.session != sessionDetails.sessionId }.lastOrNull()
+
+                            if (lastMessage != null) {
+                                lastMessageTime = lastMessage.timeStamp
+                                val reply = eliza.processInput(lastMessage.message)
+                                println("received message in room ${room.uid}")
+                                println("message: '${lastMessage.message}'")
+                                println("replying with: '${reply}'")
+                                println()
+                                chatApi.postApiRoomWithRoomid(roomId, sessionDetails.sessionToken, reply)
+                            }
+
+                            active = state.info.remainingTime > 0
+
+                            } catch (e : ClientException) {
+                                System.err.println("something went wrong:")
+                                e.printStackTrace()
+                            }
+
+                            Thread.sleep(1000)
+                        }
+
+                    }.start()
+
+                }
+            }
+            Thread.sleep(1000)
+        }
+    }
+}
